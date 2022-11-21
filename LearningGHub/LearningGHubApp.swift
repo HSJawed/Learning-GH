db39754e5752b504c695f684399e84b59fe5fcc8
Jawed <hsjawed@yahoo.com>
2022-11-21 12:22:30 +0600

    Initial Commit

---
+++ LearningGHub/LearningGHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHubApp.swift
+//  LearningGHub
+//
+//  Created by MD HUSSAIN SHAH JAWED on 21/11/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
